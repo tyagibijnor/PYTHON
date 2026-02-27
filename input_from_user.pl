370a87cb64e847fd467013c4ff42f7679d65ea3e
NAVNEET\tyagi <tyagibijnor@gmail.com>
2026-02-27 16:34:56 +0530

    main: added method for math ops

--- input_from_user.pl
+++ input_from_user.pl
@@ -1,2 +1,30 @@
+## Testing user input case
 name=input("Enter your name : ")
 print(' Welcome ',name) 
+print(type(name))
+## Math operations
+def arithmetic_operation():
+    try:
+        num1 = float(input("Enter the first number: "))
+        operator = input("Enter an operator (+, -, *, /): ")
+        num2 = float(input("Enter the second number: "))
+
+        if operator == '+':
+            result = num1 + num2
+        elif operator == '-':
+            result = num1 - num2
+        elif operator == '*':
+            result = num1 * num2
+        elif operator == '/':
+            if num2 == 0:
+                return "Error: Division by zero is not allowed."
+            result = num1 / num2
+        else:
+            return "Invalid operator."
+
+        return f"The result of {num1} {operator} {num2} is {result}"
+
+    except ValueError:
+        return "Invalid input. Please enter numeric values."
+
+print(arithmetic_operation())
