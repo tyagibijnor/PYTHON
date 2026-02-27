60868792254706da68b4fe8bef73bed43f05d37c
NAVNEET\tyagi <tyagibijnor@gmail.com>
2026-02-27 19:03:55 +0530

    Changes for file

---
+++ data_structure.py
@@ -0,0 +1,20 @@
+## Datastructure in python
+# List
+my_list = [1, 2, 3, 4, 5]
+# Tuple
+my_tuple = (1, 2, 3, 4, 5,5)
+# Set
+my_set = {1, 2, 3, 4, 5, 5}
+# Dictionary
+my_dict = {'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5}
+# String
+my_string = "Hello, World!"
+
+print ("List:", my_list)
+print ("Tuple:", my_tuple)
+print ("Set:", my_set)
+print ("Dictionary:", my_dict)
+print ("String:", my_string)
+print("Split List:",my_list[0:2])
+print("Split List:",my_list[:3])
+print("Split List:",my_list[-2])
